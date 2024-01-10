b11faa952d00ac94e58aacaf66013b05acae11fd
MSP20086 <mihirpandit04@gmail.com>
2024-01-10 23:43:22 +0530

    Stylizing

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { Button, Card, Label, TextInput } from 'flowbite-react';
 import { useLogin } from '../hooks/useLogin';
 import { useNavigate } from 'react-router-dom';
-import srcimg from '../assets/image.jpg';
+import srcimg from '../assets/best.jpg';
+import '../styles/colorgradient.css';
 
 
 const Login = () => {
@@ -27,30 +28,30 @@ const Login = () => {
     };
 
     return (
-        <div className='w-screen h-screen flex'>
-            <div className='grid grid-cols-1 md:grid-cols-2 m-auto h-[550px] shadow-lg shadow-gray-600 sm:max-w-[900px]'>
+        <div className='w-screen h-screen flex bg-gradient-to-br from-cyan-50 via-white to-gray-300 background-animate'>
+            <div className='grid grid-cols-1 md:grid-cols-2 m-auto h-[550px] shadow-lg shadow-gray-600 sm:max-w-[900px] align-middle items-center border-black'>
                 <div className='w-full h-[550px] hidden md:block'>
                     <img src={srcimg} alt="" className='w-full h-full' />
                 </div>
-                <Card className="flex p-4 flex-col justify-around items-center">
+                <Card className="flex p-4 flex-col justify-around items-center align-middle">
                     <form className="flex flex-col" onSubmit={handleSubmit}>
-                        <h2 className='text-center text-4xl font-bold mb-8'>LOG IN</h2>
+                        <h2 className='text-center text-4xl font-bold mb-8 text-black'>LOG IN</h2>
                         <div>
-                            <div className="mb-2 block text-center">
-                                <Label htmlFor="email1" value="Your email" />
+                            <div className="mb-2 block">
+                                <Label htmlFor="email1" value="Your email" className='text-black' />
                             </div>
-                            <TextInput id="email1" type="email" value={email} placeholder="name@flowbite.com" required onChange={(e) => setEmail(e.target.value)} className='text-black' />
+                            <TextInput id="email" type="email" value={email} placeholder="mario@gmail.com" required onChange={(e) => setEmail(e.target.value)} className='text-black' />
                         </div>
-                        <div>
-                            <div className="mb-2 block text-center">
-                                <Label htmlFor="password1" value="Your password" />
+                        <div className='py-4'>
+                            <div className="mb-2 block">
+                                <Label htmlFor="password1" value="Your password" className='text-black' />
                             </div>
                             <TextInput id="password1" type="password" value={password} required onChange={(e) => setPassword(e.target.value)} className='text-black' />
                         </div>
-                        <div className='text-center mb-2 block'>
-                            <Button type="submit" disabled={isLoading} className=' bg-teal-700 hover:bg-teal-900 w-full p-2'>Login</Button>
+                        <div className='text-center flex justify-center'>
+                            <Button type="submit" disabled={isLoading} className=' bg-teal-700 hover:bg-teal-900 text-center mx-auto h-[40px]'>Login</Button>
                         </div>
-                        <div className='text-center mb-2 block'>
+                        <div className='text-center mb-1 block'>
                             <Button onClick={redirectToSignup} className='text-teal-700 hover:text-teal-900 w-full p-2'>Don't have an account ? Sign up</Button>
                         </div>
                         {error && (
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Button, Card, Label, TextInput } from 'flowbite-react';
 import { useSignup } from '../hooks/useSignup';
 import { useNavigate } from 'react-router-dom';
-import srcimg from '../assets/image.jpg';
+import srcimg from '../assets/best.jpg';
 
 const Signup = () => {
     const [username, setUsername] = useState('');
@@ -26,34 +26,34 @@ const Signup = () => {
     };
 
     return (
-        <div className='w-screen h-screen flex'>
-            <div className='grid grid-cols-1 md:grid-cols-2 m-auto h-[550px] shadow-lg shadow-gray-600 sm:max-w-[900px]'>
+        <div className='w-screen h-screen flex bg-gradient-to-br from-cyan-50 via-white to-gray-300 background-animate'>
+            <div className='grid grid-cols-1 md:grid-cols-2 m-auto h-[550px] shadow-lg shadow-gray-600 sm:max-w-[900px] align-middle items-center'>
                 <div className='w-full h-[550px] hidden md:block'>
                     <img src={srcimg} alt="" className='w-full h-full' />
                 </div>
-                <Card className="flex p-4 flex-col justify-around items-center">
+                <Card className="flex p-4 flex-col justify-around items-center align-middle bg-opacity-5">
                     <form className="flex flex-col" onSubmit={handleSubmit}>
-                        <h2 className='text-center text-4xl font-bold mb-8'>SIGN UP</h2>
+                        <h2 className='text-center text-4xl font-bold mb-8 text-black'>SIGN UP</h2>
                         <div>
-                            <div className="mb-2 block text-center">
-                                <Label htmlFor="username1" value="Your username" />
+                            <div className="mb-2 block">
+                                <Label htmlFor="username1" value="Your username" className='text-black' />
                             </div>
-                            <TextInput id="username" value={username} type="username" placeholder='mario' required onChange={(e) => setUsername(e.target.value)} className='text-black p-2' />
+                            <TextInput id="username" value={username} type="email" placeholder="mario" required onChange={(e) => setUsername(e.target.value)} className='text-black' />
                         </div>
                         <div>
-                            <div className="mb-2 block text-center">
-                                <Label htmlFor="email1" value="Your email" />
+                            <div className="mb-2 block">
+                                <Label htmlFor="email1" value="Your email" className='text-black' />
                             </div>
-                            <TextInput id="email" value={email} type="email" placeholder="mario@gmail.com" required onChange={(e) => setEmail(e.target.value)} className='text-black p-2' />
+                            <TextInput id="email" value={email} type="email" placeholder="mario@gmail.com" required onChange={(e) => setEmail(e.target.value)} className='text-black' />
                         </div>
                         <div>
-                            <div className="mb-2 block text-center">
-                                <Label htmlFor="password1" value="Your password" />
+                            <div className="mb-2 block">
+                                <Label htmlFor="password1" value="Your password" className='text-black' />
                             </div>
-                            <TextInput id="password" value={password} type="password" required onChange={(e) => setPassword(e.target.value)} className='text-black p-2' />
+                            <TextInput id="password" value={password} type="password" required onChange={(e) => setPassword(e.target.value)} className='text-black' />
                         </div>
-                        <div className='text-center mb-2 block'>
-                            <Button type="submit" disabled={isLoading} className=' bg-teal-700 hover:bg-teal-900 w-full p-2'>Submit</Button>
+                        <div className='text-center mb-2 p-2'>
+                            <Button type="submit" disabled={isLoading} className=' bg-teal-700 hover:bg-teal-900  p-2 mx-auto h-[40px]'>Submit</Button>
                         </div>
                         <div className='text-center mb-2 block'>
                             <Button onClick={redirectToLogin} className='text-teal-700 hover:text-teal-900 w-full p-2 text-center'>Already have an account? Login</Button>
