0298d5546bf055bcc290a63b2226b3ef62475805
MSP20086 <mihirpandit04@gmail.com>
2024-01-31 17:05:06 +0530

    ig most issues are now solved

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,34 @@
-import React from "react";
-import { Link } from 'react-router-dom';
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import '../styles/colorgradient.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero';
+import Gallery from './components/Gallery';
+import Checklist from './components/Checklist';
+import Testimonials from './components/Testimonials';
+import Carousel from './components/Carousel';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import './styles/colorgradient.css';
 
-export default function Home() {
+
+
+function App() {
   return (
-    <div className=" h-full w-screen  bg-gradient-to-br from-cyan-100 via-white to-gray-300 background-animate">
+    <div className='bg-gradient-to-br from-[#E7D4FA] via-white to-gray-300 background-animate w-screen h-full'>
       <Navbar />
-      <Link to={'/form'} className="relative top-48 " >Form</Link>
-      <Footer />
+      <div className='bg-gradient-to-br from-[#A742CC] via-white to-gray-300 background-animate w-screen h-full'>
+        <Hero />
+        <Link to={'/form'} className="relative top-48 " >Form</Link>
+
+        <Gallery />
+
+        <Testimonials />
+
+        <Banner />
+        <Footer />
+      </div>
+
     </div>
-  )}
+  );
+}
+
+export default App;
