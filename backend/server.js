96bd7572de97f74f1a707a9afe3551b666de5ddf
MSP20086 <mihirpandit04@gmail.com>
2024-02-09 13:07:58 +0530

    good luck

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,13 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+})
+
 
 app.use(bodyParser.json());
 
