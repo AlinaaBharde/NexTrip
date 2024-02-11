5e2da615a3e8dba0954e968c15a862f7c25ecd4a
MSP20086 <mihirpandit04@gmail.com>
2024-02-11 08:48:02 +0530

    --

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,7 +7,8 @@ export default function footer() {
   const Year = new Date().getFullYear();
 
   return (
-    <div className="flex flex-col bg-[#f5f5f5]">
+    <div className="flex flex-col min-h-screen">
+      <div className='flex-grow' />
       <Footer container className='pt-4 pl-6 pb-6 pr-6 z-20 drop-shadow-lg border w-full bottom-0' style={{
         backgroundColor: 'white'
       }}>
